8116a906e247091cdbf03dcbd4536bdaeda5d30a
Xxtorres <jsetorres1997@gmail.com>
2026-08-04 21:02:17 -0300

    sex

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,7 +50,6 @@ export default function RootLayout({
   return (
     <html lang="es" className="dark">
       <head>
-        <link rel="manifest" href="/manifest.json" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
